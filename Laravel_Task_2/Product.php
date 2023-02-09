9c5d586af9e9e46ad91784713527ab17887bc273
AliHussein <alihusseiinali@gmail.com>
2023-02-09 20:00:00 +0200

    laravel_tasks1,2,3

---
+++ Laravel_Task_2/Product.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class product extends Model
+{
+    use HasFactory;
+    public static $ruels = ['name' => 'required'];
+
+    protected $guarded = ['rating', 'rating_count'];
+
+    public function category(){
+        return $this->belongsTo(Category::class);
+    }
+}
